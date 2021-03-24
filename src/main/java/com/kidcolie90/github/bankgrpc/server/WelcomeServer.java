313aa3c9f4cdb561042dfb08621a9e0ef1056bcb
col lewis <kidcolie90@gmail.com>
2021-03-24 19:15:38 +0000

    code now includes grpc server and client and proto files

---
+++ src/main/java/com/kidcolie90/github/bankgrpc/server/WelcomeServer.java
@@ -0,0 +1,29 @@
+package com.kidcolie90.github.bankgrpc.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+
+public class WelcomeServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("Hi, Welcome to the gRPC Bank");
+
+        Server server = ServerBuilder.forPort(50051)
+                .build(); //builing server on port 50051
+
+        server.start();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() ->{
+            System.out.println("ShutDown Req Noted");
+                    server.shutdown(); //shuts down the server and allows triggers "server.awaittermination to run"
+                    System.out.println("server shutdown");
+
+        }));
+
+        server.awaitTermination(); //ensures application does not terminate on run after server shutdown request received and prog has completed
+
+
+    }
+}
